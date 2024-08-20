7d6120b79758a456dbc2025ed23dfc76d356b0e7
sharanu7781 <mitecsharanu@gmail.com>
2024-08-20 13:19:02 +0530

    Register & login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('login', 'Home::index');
+$routes->get('register', 'Home::register');
+$routes->post('register', 'Home::saveregister');
 $routes->get('Employeedashboard', 'Home::Employeedashboard');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,15 +1,42 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\RegisterModel;
 
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('login');
     }
     public function Employeedashboard()
     {
         echo view('Employee/Dashboard');
     }
+    public function register()
+    {
+
+        
+        echo view('register');
+    }
+
+    public function saveregister()
+    {
+        $db = \Config\Database::connect();
+        $data = [
+            
+            // 'company_name' => $this->request->getPost('company_name'),
+    
+            'frist_name' => $this->request->getPost('frist_name'),
+            'last_name'  => $this->request->getPost('last_name'),
+            'email'      => $this->request->getPost('email'),
+            'password'   => $this->request->getPost('password'),
+            'role'       => $this->request->getPost('role'),
+        ];
+    
+        // Insert data into the database table
+        $db->table('tbl_register')->insert($data);
+        return redirect()->to('login');
+    }
+    
 }
